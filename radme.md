16bdf3d4283a69a8ac5851e62d4634854506f024
samian <sam.iannazzo@yahoo.com>
2025-11-08 12:49:24 -0800

    Update 2 readme for function to display array / added update 3 for function to display the sum of numbers in the array

--- radme.md
+++ radme.md
@@ -2,3 +2,7 @@ Edited by: Samuel Iannazzo
 Purpose: To demonstrate understanding of arrays.
 - Update 1:
 Created readme file to upload to github.
+- Update 2:
+created a function to display an array of numbers 1-10 in order. Used for and return to display this array.
+- Update 3:
+Added a let a = and let sum.reduce to find the sum of all the numbers in the a array. 
--- script.js
+++ script.js
@@ -2,12 +2,23 @@
 // containing all the numbers from start up to (and including) end.
 // Example output:
 // console.log(range(1, 10)); // outputs [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
+function range(start, end) {
+    let result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    for (let i = start; i <= end; i++) {
+        result.push(i);
+    }
+    return result;
+}
+console.log(range(1, 10));
 // 2. Next, write a sum function that takes an array of numbers and returns the sum of
 // these numbers. Run the example program and see whether it does indeed return 55.
 // Example output:
 // console.log(sum(range(1, 10))); // returns 55
+let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+let sum = a.reduce( (acc,e) => acc + e, 0)
+
+console.log("Sum of Array Elements: ", sum);
 // 3. Arrays have a reverse method that changes the array by inverting the order in which
 // its elements appear. For this exercise, write two functions, reverseArray and
 // reverseArrayInPlace. The first, reverseArray, takes an array as an argument and
